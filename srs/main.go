f783d0ff48bbf3257c027b2338018686ea4df2f8
winlin <winlin@vip.126.com>
2015-10-21 18:12:42 +0800

    replace space with tab.

--- srs/main.go
+++ srs/main.go
@@ -24,52 +24,52 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package main
 
 import (
-    "os"
-    "flag"
-    "github.com/simple-rtmp-server/go-srs/core"
-    "fmt"
+        "os"
+        "flag"
+        "github.com/simple-rtmp-server/go-srs/core"
+        "fmt"
 )
 
 // the startup argv:
-//      -c conf/srs.json
-//      --c conf/srs.json
-//      -c=conf/srs.json
-//      --c=conf/srs.json
+//          -c conf/srs.json
+//          --c conf/srs.json
+//          -c=conf/srs.json
+//          --c=conf/srs.json
 var confFile = flag.String("c", "conf/srs.json", "the config file.")
 
 func run() int {
-    flag.Parse()
+        flag.Parse()
 
-    svr := core.NewServer()
-    defer svr.Close()
+        svr := core.NewServer()
+        defer svr.Close()
 
-    if err := svr.ParseConfig(*confFile); err != nil {
-        core.GsError.Println("parse config from", *confFile, "failed, err is", err)
-        return -1
-    }
+        if err := svr.ParseConfig(*confFile); err != nil {
+                core.GsError.Println("parse config from", *confFile, "failed, err is", err)
+                return -1
+        }
 
-    if err := svr.PrepareLogger(); err != nil {
-        core.GsError.Println("prepare logger failed, err is", err)
-        return -1
-    }
+        if err := svr.PrepareLogger(); err != nil {
+                core.GsError.Println("prepare logger failed, err is", err)
+                return -1
+        }
 
-    core.GsTrace.Println("Copyright (c) 2013-2015 SRS(simple-rtmp-server)")
-    core.GsTrace.Println(fmt.Sprintf("GO-SRS/%v is a golang implementation of SRS.", core.Version()))
+        core.GsTrace.Println("Copyright (c) 2013-2015 SRS(simple-rtmp-server)")
+        core.GsTrace.Println(fmt.Sprintf("GO-SRS/%v is a golang implementation of SRS.", core.Version()))
 
-    if err := svr.Initialize(); err != nil {
-        core.GsError.Println("initialize server failed, err is", err)
-        return -1
-    }
+        if err := svr.Initialize(); err != nil {
+                core.GsError.Println("initialize server failed, err is", err)
+                return -1
+        }
 
-    if err := svr.Run(); err != nil {
-        core.GsError.Println("run server failed, err is", err)
-        return -1
-    }
+        if err := svr.Run(); err != nil {
+                core.GsError.Println("run server failed, err is", err)
+                return -1
+        }
 
-    return 0
+        return 0
 }
 
 func main() {
-    ret := run()
-    os.Exit(ret)
+        ret := run()
+        os.Exit(ret)
 }
