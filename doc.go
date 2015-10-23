c29dfe4914e4025cfcac44247fbed93e9775b4ae
winlin <winlin@vip.126.com>
2015-10-23 18:11:56 +0800

    update doc of go-srs

--- doc.go
+++ doc.go
@@ -1,13 +1,10 @@
 /*
 The go-srs project is a go implementation of http://github.com/simple-rtmp-server/srs.
+
+Please use the following components:
+    1. srs: the srs server.
+
+For example:
+    go build -o objs/srs github.com/simple-rtmp-server/go-srs/srs && ./objs/srs -c conf/console.json
 */
 package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("Please use the following components:")
-    fmt.Println("   1. srs: the srs server.")
-    fmt.Println("For example:")
-    fmt.Println("   go build -o objs/srs github.com/simple-rtmp-server/go-srs/srs && ./objs/srs -c conf/console.json")
-}
