f99300aa926fd146bc0e3b31eceb7f12a1e22c80
winlin <winlin@vip.126.com>
2015-11-04 16:25:52 +0800

    support daemon

--- app/daemon_unix.go
+++ app/daemon_unix.go
@@ -25,12 +25,51 @@
 
 package app
 
-import "github.com/simple-rtmp-server/go-srs/core"
+import (
+	"fmt"
+	"github.com/simple-rtmp-server/go-srs/core"
+	"os"
+	"runtime"
+	"syscall"
+)
 
 func (s *Server) daemon() error {
+	// set to single thread mode.
+	runtime.GOMAXPROCS(1)
+
+	// lock goroutine to thread.
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	// start in daemon if possible.
+	pid, r2, err := syscall.RawSyscall(syscall.SYS_FORK, 0, 0, 0)
+
+	if err != 0 {
+		return fmt.Errorf("fork failed, err is %v", err)
+	}
+
+	if r2 < 0 {
+		return fmt.Errorf("fork failed, r2 is", r2)
+	}
+
+	// for darwin, the child process forked.
+	if runtime.GOOS == "darwin" && r2 == 1 {
+		pid = 0
+	}
+
+	// exit parent process.
+	if pid > 0 {
+		os.Exit(0)
+	}
+
+	// setsid for child process to daemon.
+	if _, err := syscall.Setsid(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *Server) daemonOnRunning() {
-	core.GsTrace.Println("server run in daemon.")
+	core.GsTrace.Println("server run in daemon, pid is", os.Getpid(), "and ppid is", os.Getppid())
 }
