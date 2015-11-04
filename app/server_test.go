cb0d7a113cc75a794cdcada129f0997c693ba91f
winlin <winlin@vip.126.com>
2015-11-04 11:19:05 +0800

    add example for worker container

---
+++ app/server_test.go
@@ -0,0 +1,49 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013-2015 SRS(simple-rtmp-server)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package app
+
+import "time"
+
+func ExampleWorkerContainer() {
+	var wc WorkerContainer
+	wc.GFork(func(wc WorkerContainer) {
+		for {
+			select {
+			case <-time.After(3 * time.Duration):
+				// select other channel, do something to get error.
+				if err := error(nil); err != nil {
+					// when got none-recoverable error, notify container to quit.
+					wc.Quit()
+					return
+				}
+			case <-wc.QC():
+				// when got a quit signal, break the loop.
+				// and must notify the container again for other workers
+				// in container to quit.
+				wc.Quit()
+				return
+			}
+		}
+	})
+}
