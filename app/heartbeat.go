09034a09ce00cedf640c863ee67ee2ca852215b2
winlin <winlin@vip.126.com>
2015-11-10 15:51:23 +0800

    refine the ip fetch function.

--- app/heartbeat.go
+++ app/heartbeat.go
@@ -98,6 +98,22 @@ func (h *Heartbeat) discovery() (err error) {
 	h.lock.Lock()
 	defer h.lock.Unlock()
 
+	// check whether the ip is ok to export.
+	vf := func(ip net.IP) bool {
+		return ip != nil && ip.To4() != nil && !ip.IsLoopback()
+	}
+
+	// fetch the ip from addr interface.
+	ipf := func(addr net.Addr) (string, bool) {
+		if v, ok := addr.(*net.IPNet); ok && vf(v.IP) {
+			return v.IP.String(), true
+		} else if v, ok := addr.(*net.IPAddr); ok && vf(v.IP) {
+			return v.IP.String(), true
+		} else {
+			return "", false
+		}
+	}
+
 	var ifaces []net.Interface
 	if ifaces, err = net.Interfaces(); err != nil {
 		return
@@ -109,16 +125,11 @@ func (h *Heartbeat) discovery() (err error) {
 			return
 		}
 
+		// dumps all network interfaces.
 		for _, addr := range addrs {
-			vf := func(ip net.IP) bool {
-				return ip != nil && ip.To4() != nil && !ip.IsLoopback()
-			}
-			if v, ok := addr.(*net.IPNet); ok && vf(v.IP) {
-				core.GsTrace.Println("iface", iface.Name, "ip is", v.IP.String())
-				h.ips = append(h.ips, v.IP.String())
-			} else if v, ok := addr.(*net.IPAddr); ok && vf(v.IP) {
-				core.GsTrace.Println("iface", iface.Name, "ip is", v.IP.String())
-				h.ips = append(h.ips, v.IP.String())
+			if v, ok := ipf(addr); ok {
+				core.GsTrace.Println("iface", iface.Name, "ip is", v)
+				h.ips = append(h.ips, v)
 			} else {
 				core.GsInfo.Println("iface", iface.Name, addr, reflect.TypeOf(addr))
 			}
