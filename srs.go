8c982fe54fe68c4a9ca31dcae4da52f331cb5c0a
winlin <winlin@vip.126.com>
2015-11-12 12:20:04 +0800

    rename go-srs to go-oryx

--- srs.go
+++ srs.go
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2013-2015 SRS(ossrs)
+// Copyright (c) 2013-2015 Oryx(ossrs)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -53,7 +53,7 @@ func serve(svr *app.Server) int {
 
 	srsMain(svr)
 
-	core.GsTrace.Println("Copyright (c) 2013-2015 SRS(ossrs)")
+	core.GsTrace.Println("Copyright (c) 2013-2015 Oryx(ossrs)")
 	core.GsTrace.Println(fmt.Sprintf("GO-SRS/%v is a golang implementation of SRS.", core.Version()))
 
 	if err := svr.Initialize(); err != nil {
