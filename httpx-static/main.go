02df6bb61aaae1f616ff94552628f5ba9c227937
winlin <winlin@vip.126.com>
2017-11-03 11:38:15 +0800

    Support multiple proxy HTTP to HTTPS

--- httpx-static/main.go
+++ httpx-static/main.go
@@ -28,10 +28,13 @@ SOFTWARE.
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"flag"
 	"fmt"
+	oe "github.com/ossrs/go-oryx-lib/errors"
 	"github.com/ossrs/go-oryx-lib/https"
+	ol "github.com/ossrs/go-oryx-lib/logger"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -42,17 +45,27 @@ import (
 	"sync"
 )
 
-const server = "Oryx/0.0.2"
+const server = "Oryx/0.0.3"
 
-func main() {
+type Proxies []string
+
+func (v *Proxies) String() string {
+	return "proxy to backend services"
+}
+
+func (v *Proxies) Set(value string) error {
+	*v = append(*v, value)
+	return nil
+}
+
+func run(ctx context.Context) error {
 	fmt.Println(server, "HTTP/HTTPS static server with API proxy.")
 
-	var err error
 	var httpPort, httpsPort int
 	var httpsDomains, html, cacheFile string
 	var useLetsEncrypt bool
 	var ssCert, ssKey string
-	var oproxy string
+	var oproxies Proxies
 	flag.IntVar(&httpPort, "http", 0, "http listen at. 0 to disable http.")
 	flag.IntVar(&httpsPort, "https", 0, "https listen at. 0 to disable https. 443 to serve. ")
 	flag.StringVar(&httpsDomains, "domains", "", "the allow domains, empty to allow all. for example: ossrs.net,www.ossrs.net")
@@ -61,36 +74,47 @@ func main() {
 	flag.BoolVar(&useLetsEncrypt, "lets", false, "whether use letsencrypt CA. self sign if not.")
 	flag.StringVar(&ssKey, "ssk", "server.key", "https self-sign key by(before server.cert): openssl genrsa -out server.key 2048")
 	flag.StringVar(&ssCert, "ssc", "server.crt", "https self-sign cert by: openssl req -new -x509 -key server.key -out server.crt -days 365")
-	flag.StringVar(&oproxy, "proxy", "", "proxy the matched path to backend, for example, -proxy http://127.0.0.1:8888/api/webrtc")
+	flag.Var(&oproxies, "proxy", "one or more proxy the matched path to backend, for example, -proxy http://127.0.0.1:8888/api/webrtc")
 	flag.Parse()
 
 	if useLetsEncrypt && (httpsPort != 0 && httpsPort != 443) {
-		fmt.Println("https must be 0(disabled) or 443(enabled)")
-		os.Exit(-1)
+		return oe.Errorf("for letsencrypt, https=%v must be 0(disabled) or 443(enabled)", httpsPort)
 	}
 	if httpPort == 0 && httpsPort == 0 {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
 
-	var proxyUrl *url.URL
-	var proxy *httputil.ReverseProxy
-	if oproxy != "" {
-		if proxyUrl, err = url.Parse(oproxy); err != nil {
-			fmt.Println("proxy is not legal url, proxy is", oproxy)
-			os.Exit(-1)
+	var proxyUrls []*url.URL
+	proxies := map[string]*httputil.ReverseProxy{}
+	for _, oproxy := range []string(oproxies) {
+		if oproxy == "" {
+			return oe.Errorf("empty proxy in %v", oproxies)
 		}
 
-		proxy = &httputil.ReverseProxy{
+		proxyUrl, err := url.Parse(oproxy)
+		if err != nil {
+			return oe.Wrapf(err, "parse proxy %v", oproxy)
+		}
+
+		proxy := &httputil.ReverseProxy{
 			Director: func(r *http.Request) {
 				r.URL.Scheme = proxyUrl.Scheme
 				r.URL.Host = proxyUrl.Host
 				if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
 					r.Header.Set("X-Real-IP", ip)
 				}
-				//fmt.Println(fmt.Sprintf("proxy http %v to %v", r.RemoteAddr, r.URL.String()))
+				//ol.Tf("proxy http %v to %v", r.RemoteAddr, r.URL.String())
 			},
 		}
+
+		if _, ok := proxies[proxyUrl.Path]; ok {
+			return oe.Errorf("proxy %v duplicated", proxyUrl.Path)
+		}
+
+		proxyUrls = append(proxyUrls, proxyUrl)
+		proxies[proxyUrl.Path] = proxy
+		ol.Tf(ctx, "Proxy %v to %v", proxyUrl.Path, oproxy)
 	}
 
 	if !path.IsAbs(cacheFile) && path.IsAbs(os.Args[0]) {
@@ -111,32 +135,38 @@ func main() {
 			w.Header().Set("Access-Control-Allow-Headers", "origin,range,accept-encoding,referer,Cache-Control,X-Proxy-Authorization,X-Requested-With,Content-Type")
 		}
 
-		if proxyUrl == nil {
+		if proxyUrls == nil {
 			fs.ServeHTTP(w, r)
 			return
 		}
 
-		srcPath,proxyPath := r.URL.Path,proxyUrl.Path
-		if !strings.HasSuffix(srcPath, "/") {
-			// /api to /api/
-			// /api.js to /api.js/
-			// /api/100 to /api/100/
-			srcPath += "/"
-		}
-		if !strings.HasSuffix(proxyPath, "/") {
-			// /api/ to /api/
-			// to match /api/ or /api/100
-			// and not match /api.js/
-			proxyPath += "/"
-		}
-		if strings.HasPrefix(srcPath, proxyPath) {
+		for _, proxyUrl := range proxyUrls {
+			srcPath, proxyPath := r.URL.Path, proxyUrl.Path
+			if !strings.HasSuffix(srcPath, "/") {
+				// /api to /api/
+				// /api.js to /api.js/
+				// /api/100 to /api/100/
+				srcPath += "/"
+			}
+			if !strings.HasSuffix(proxyPath, "/") {
+				// /api/ to /api/
+				// to match /api/ or /api/100
+				// and not match /api.js/
+				proxyPath += "/"
+			}
+			if !strings.HasPrefix(srcPath, proxyPath) {
+				continue
+			}
+
 			// For matched OPTIONS, directly return without response.
 			if r.Method == "OPTIONS" {
 				return
 			}
 
-			proxy.ServeHTTP(w, r)
-			return
+			if proxy, ok := proxies[proxyUrl.Path]; ok {
+				proxy.ServeHTTP(w, r)
+				return
+			}
 		}
 
 		fs.ServeHTTP(w, r)
@@ -164,34 +194,44 @@ func main() {
 			protos = append(protos, fmt.Sprintf("self-sign(%v, %v)", ssKey, ssCert))
 		}
 	}
-	fmt.Println(fmt.Sprintf("%v html root at %v", strings.Join(protos, ", "), string(html)))
+	ol.Tf(ctx, "%v html root at %v", strings.Join(protos, ", "), string(html))
+
+	var hs, hss *http.Server
 
 	wg := sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var err error
 	go func() {
 		defer wg.Done()
-
 		if httpPort == 0 {
-			fmt.Println("http server disabled")
+			ol.W(ctx, "http server disabled")
 			return
 		}
 
-		if err := http.ListenAndServe(fmt.Sprintf(":%v", httpPort), nil); err != nil {
-			panic(err)
+		defer cancel()
+		hs = &http.Server{Addr: fmt.Sprintf(":%v", httpPort), Handler: nil}
+		ol.Tf(ctx, "http serve at %v", httpPort)
+
+		if err = hs.ListenAndServe(); err != nil {
+			err = oe.Wrapf(err, "serve http")
+			return
 		}
-		fmt.Println("http server ok.")
+		ol.T("http server ok")
 	}()
 	wg.Add(1)
 
 	go func() {
 		defer wg.Done()
-
 		if httpsPort == 0 {
-			fmt.Println("https server disabled")
+			ol.W(ctx, "https server disabled")
 			return
 		}
 
-		var m https.Manager
+		defer cancel()
 
+		var m https.Manager
 		if useLetsEncrypt {
 			var domains []string
 			if httpsDomains != "" {
@@ -199,27 +239,50 @@ func main() {
 			}
 
 			if m, err = https.NewLetsencryptManager("", domains, cacheFile); err != nil {
-				panic(err)
+				err = oe.Wrapf(err, "create letsencrypt manager")
+				return
 			}
 		} else {
 			if m, err = https.NewSelfSignManager(ssCert, ssKey); err != nil {
-				panic(err)
+				err = oe.Wrapf(err, "create self-sign manager")
+				return
 			}
 		}
 
-		svr := &http.Server{
+		hss = &http.Server{
 			Addr: fmt.Sprintf(":%v", httpsPort),
 			TLSConfig: &tls.Config{
 				GetCertificate: m.GetCertificate,
 			},
 		}
+		ol.Tf(ctx, "http serve at %v", httpsPort)
 
-		if err := svr.ListenAndServeTLS("", ""); err != nil {
-			panic(err)
+		if err = hss.ListenAndServeTLS("", ""); err != nil {
+			err = oe.Wrapf(err, "listen and serve https")
+			return
 		}
-		fmt.Println("https server ok.")
+		ol.T("https serve ok")
 	}()
 	wg.Add(1)
 
+	select {
+	case <-ctx.Done():
+		if hs != nil {
+			hs.Close()
+		}
+		if hss != nil {
+			hss.Close()
+		}
+	}
 	wg.Wait()
+
+	return err
+}
+
+func main() {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
+		ol.Ef(ctx, "run err %+v", err)
+		os.Exit(-1)
+	}
 }
