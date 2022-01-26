2eebe4ef2e2d7959a58a1503f7e22206435bec95
winlin <winlin@vip.126.com>
2022-01-26 10:56:36 +0800

    Update upgrade script

--- auto/pub.sh
+++ auto/pub.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+REALPATH=$(realpath $0)
+WORK_DIR=$(cd $(dirname $REALPATH)/.. && pwd)
+echo "Run pub at $WORK_DIR from $0"
+cd $WORK_DIR
+
+git st |grep -q 'nothing to commit'
+if [[ $? -ne 0 ]]; then
+  echo "Failed: Please commit before release";
+  exit 1
+fi
+
 RELEASE=$(git describe --tags --abbrev=0 --exclude release-*)
 REVISION=$(echo $RELEASE|awk -F . '{print $3}')
 let NEXT=$REVISION+1
